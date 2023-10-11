4e63ce7306158969f98f8b630c86a55981d28212
walicharamsey <ramsey@Cullen-PC.localdomain>
2023-10-11 08:46:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+    return winRecord ? winRecord.year : undefined;
+}
+
